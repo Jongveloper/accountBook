7c39882e4ab20a6237cdc1c8016912e3b85463f0
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-06 23:06:51 +0900

    ✨ account C.R.U.D 구현

--- server/router/accounts.js
+++ server/router/accounts.js
@@ -1,2 +1,14 @@
-import { Express } from 'express';
+import express from 'express';
 import 'express-async-errors';
+import * as accountController from '../controller/accounts.js';
+
+const router = express.Router();
+
+router.get('/account', accountController.getAccounts);
+
+router.post('/account', accountController.createAccount);
+
+router.put('/account/:id', accountController.updateAccount);
+
+router.delete('/account/:id', accountController.deleteAccount);
+export default router;
