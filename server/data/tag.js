3f9a7184c643e3b1d172327c5baea40bbec1ed9e
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-10 18:15:30 +0900

    ✨ 태그 작성(인증) 구현

--- server/data/tag.js
+++ server/data/tag.js
@@ -1,6 +1,8 @@
 import SQ from 'sequelize';
 import { sequelize } from '../db/database.js';
+import { User } from './auth.js';
 
+const Sequelize = SQ.Sequelize;
 const DataTypes = SQ.DataTypes;
 
 const Tag = sequelize.define('tag', {
@@ -19,25 +21,55 @@ const Tag = sequelize.define('tag', {
     allowNull: true,
   },
 });
+Tag.belongsTo(User);
+
+const INCLUDE_USER = {
+  attributes: [
+    'id',
+    'tagName',
+    'color',
+    'userId',
+    'createdAt',
+    [Sequelize.col('user.name'), 'name'],
+    [Sequelize.col('user.username'), 'username'],
+  ],
+  include: {
+    model: User,
+    attributes: [],
+  },
+};
 
 const ORDER_DESC = {
   order: [['createdAt', 'DESC']],
 };
 
 export async function getAll() {
-  return Tag.findAll({ ORDER_DESC });
+  return Tag.findAll({ ...INCLUDE_USER, ...ORDER_DESC });
+}
+
+export async function getAllByUsername(username) {
+  return Tag.findAll({
+    ...INCLUDE_USER,
+    ...ORDER_DESC,
+    include: {
+      ...INCLUDE_USER.include,
+      where: { username },
+    },
+  });
 }
 
 export async function getById(id) {
   return Tag.findAll({
     where: { id },
+    ...INCLUDE_USER,
   });
 }
 
-export async function create(tagName, color) {
+export async function create(tagName, color, userId) {
   return Tag.create({
     tagName,
     color,
+    userId,
   }).then((data) => this.getById(data.dataValues.id));
 }
 
