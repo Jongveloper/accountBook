53cfe003ad25059b198d2aa01d2dfb68122129a0
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-12 18:31:36 +0900

    🥅 잘못된 타입 수정

--- client/account-book/src/elements/index.tsx
+++ client/account-book/src/elements/index.tsx
@@ -2,10 +2,12 @@ import Input from './Input';
 import Grid from './Grid';
 import Button from './Button';
 import Container from './Container';
+import Section from './Section';
 
 export {
   Input,
   Grid,
   Button,
-  Container
+  Container,
+  Section
 }
