d5bcf9b961213b9049a6ca01ee570c696c19bb6d
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-13 20:28:52 +0900

    💄Home

--- client/account-book/src/pages/Home/index.tsx
+++ client/account-book/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Grid } from '../../elements'
 import Month from '../../components/Month';
 import SelectBox from '../../components/SelectBox'
+import AccountList from '../../components/AccountList';
 const Home = (): React.ReactElement => {
   return (
     <Container padding='30px'>
@@ -10,6 +11,9 @@ const Home = (): React.ReactElement => {
       </Grid>
       <Grid margin='0 0 0 -65px'>
         <SelectBox />
+      </Grid>
+      <Grid margin='20px 0 0 0'>
+        <AccountList />
       </Grid>
     </Container>
   )
