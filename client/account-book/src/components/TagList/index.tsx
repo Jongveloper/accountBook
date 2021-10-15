f9f95257ef2ffa3a2c1fd791f35576b5b6acaeae
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-15 23:34:42 +0900

    💄TagList

---
+++ client/account-book/src/components/TagList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TagListStyle } from './style';
+
+const TagList = () => {
+  return (
+    <TagListStyle>
+      <li style={{ minHeight: '20px', fontWeight: 900 }}>식품</li>
+    </TagListStyle>
+  )
+}
+
+export default TagList
