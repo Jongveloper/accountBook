d7dcd7cb83108fa18faeac345055a23c64b1a1f9
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-16 01:31:53 +0900

    🎨 Month 컴포넌트 재활용성 높이기

--- client/account-book/src/components/Month/index.tsx
+++ client/account-book/src/components/Month/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 import MonthStyle from './style';
-
+import { Grid } from '../../elements';
 const Month = () => {
-  return (
+  const path: string = useLocation().pathname
+
+  return path.includes('/home') ? (
     <MonthStyle>
       <h1 style={{ marginTop: '40px' }}>1월</h1>
     </MonthStyle>
-  )
+  ) :
+    <Grid>
+      <MonthStyle>
+        <h1 style={{ color: 'black', fontWeight: 700, marginTop: '20px', fontSize: '14px', minHeight: '35px' }}>가장 많은 지출</h1>
+        <p style={{ color: 'red' }}>식품</p>
+      </MonthStyle>
+    </Grid>
 }
 
 export default Month;
