3238205f1a0c111efbf6aae87a10987b51b741a9
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-16 00:13:44 +0900

    🚧Info router연결

--- client/account-book/src/pages/Info/index.tsx
+++ client/account-book/src/pages/Info/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Container, Grid } from '../../elements';
 import TagForm from '../../components/TagForm';
-
+import TagList from '../../components/TagList';
 const Info = (): React.ReactElement => {
   return (
-    <Container padding='20px'>
-      <Grid width='100%' margin='30px auto'>
+    <Container padding='30px'>
+      <Grid width='100%' margin='20px auto'>
         <TagForm />
+      </Grid>
+      <Grid margin='auto'>
+        <TagList />
       </Grid>
     </Container>
   )
