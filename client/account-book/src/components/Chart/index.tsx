59006c3a2378599f2a70aaa05f5cb0e191726846
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-15 19:01:02 +0900

    💄PieChart

---
+++ client/account-book/src/components/Chart/index.tsx
@@ -0,0 +1,52 @@
+import { PieChart } from 'react-minimal-pie-chart';
+import { Grid } from '../../elements'
+import { css } from 'styled-components'
+const Chart = () => {
+  const data = [
+    { title: '식품', value: 30, color: '#003f5c', income: '2000000' },
+    { title: '게임', value: 15, color: '#58508d', spend: '30000' },
+    { title: '컴퓨터', value: 20, color: '#ffa600', income: '5000000' }
+  ]
+  return (
+    <>
+      <Grid textAlign='center' margin='auto' >
+        <Grid
+          textAlign='left'
+        >
+          <h2 style={{ fontWeight: 'bold', fontSize: '25px' }}>통계</h2>
+        </Grid>
+        <PieChart
+          style={{ width: '200px', marginTop: '35px' }}
+          data={data}
+          lineWidth={70}
+          background='#f3f3f3'
+          lengthAngle={360}
+          animate
+          label={({ dataEntry }) => dataEntry.title}
+          labelStyle={{
+            fontSize: '6px',
+            fill: 'white',
+            fontWeight: 'bold'
+          }}
+        />
+      </Grid>
+      <Grid margin='35px 0 0 0'>
+        {data.map((tag, idx) => {
+          return (
+            <Grid isFlex
+              addstyle={() => {
+                return css`
+              justify-content: space-between;
+            `;
+              }}>
+              <p key={idx} style={{ fontWeight: 'bold', color: tag.color, minHeight: '30px', marginRight: '160px' }}>{tag.title}</p>
+              <p key={idx} style={{ color: 'black', minHeight: '30px', fontWeight: 'bold' }}>{tag.income ? tag.income : tag.spend}</p>
+            </Grid>
+          )
+        })}
+      </Grid>
+    </>
+  )
+}
+
+export default Chart;
